0e4757630d768779f3b7a6b9153a86b998608c6b
heitorsouza <heitorhenriquedias@gmail.com>
2025-02-09 20:13:02 -0300

    refactor: adapting the cli comments

--- socialMedia/cmd/cli.go
+++ socialMedia/cmd/cli.go
@@ -9,12 +9,7 @@ import (
 var cliCmd = &cobra.Command{
 	Use:   "cli",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `A longer description`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello World!")
 	},
